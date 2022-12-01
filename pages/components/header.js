91b2e403140f7875661d192506b22c5a7662585d
minsoo <als494@naver.com>
2022-12-02 02:16:56 +0900

    feat: Link 연결

--- pages/components/header.js
+++ pages/components/header.js
@@ -1,29 +1,40 @@
+import Link from "next/link";
+
 export default function Header() {
   return (
     <header className="text-gray-600 body-font">
       <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-        <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="currentColor"
-            className="w-10 h-10 text-white p-2 bg-blue-500 rounded-full"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M12 18v-5.25m0 0a6.01 6.01 0 001.5-.189m-1.5.189a6.01 6.01 0 01-1.5-.189m3.75 7.478a12.06 12.06 0 01-4.5 0m3.75 2.383a14.406 14.406 0 01-3 0M14.25 18v-.192c0-.983.658-1.823 1.508-2.316a7.5 7.5 0 10-7.517 0c.85.493 1.509 1.333 1.509 2.316V18"
-            />
-          </svg>
+        <Link href="/" legacyBehavior>
+          <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              className="w-10 h-10 text-white p-2 bg-blue-500 rounded-full"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M12 18v-5.25m0 0a6.01 6.01 0 001.5-.189m-1.5.189a6.01 6.01 0 01-1.5-.189m3.75 7.478a12.06 12.06 0 01-4.5 0m3.75 2.383a14.406 14.406 0 01-3 0M14.25 18v-.192c0-.983.658-1.823 1.508-2.316a7.5 7.5 0 10-7.517 0c.85.493 1.509 1.333 1.509 2.316V18"
+              />
+            </svg>
 
-          <span className="ml-3 text-xl font-bold">민수의 포트폴리오</span>
-        </a>
+            <span className="ml-3 text-xl font-bold">민수의 포트폴리오</span>
+          </a>
+        </Link>
         <nav className="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
-          <button className="mr-5 hover:text-gray-900">홈</button>
-          <button className="mr-5 hover:text-gray-900">프로젝트</button>
-          <button className="mr-5 hover:text-gray-900">연락하기</button>
+
+          <Link href="/" legacyBehavior>
+            <a className="mr-5 hover:text-gray-900">홈</a>
+          </Link>
+          <Link href="/projects" legacyBehavior>
+            <a className="mr-5 hover:text-gray-900">프로젝트</a>
+          </Link>
+          <Link href="/contect" legacyBehavior>
+            <a className="mr-5 hover:text-gray-900">연락하기</a>
+          </Link>
         </nav>
         <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">
           Button
---
+++ pages/projects.js
@@ -0,0 +1,9 @@
+import Layout from "./components/layout";
+
+export default function Projects() {
+  return (
+    <Layout>
+      <h1>projects</h1>
+    </Layout>
+  );
+}
