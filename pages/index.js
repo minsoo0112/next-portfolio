7f4a621109c6986b27c8d9f218c26e6e490f3837
minsoo <als494@naver.com>
2022-12-02 00:44:24 +0900

    feat: tailwind 설정

--- pages/index.js
+++ pages/index.js
@@ -10,9 +10,9 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
-        <h1>main</h1>
-      </main>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
       
     </Layout>
   )
