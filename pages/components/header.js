2a90cbbda775a7e2b87516dcc7e3dc4234b793f8
minsoo <als494@naver.com>
2022-12-03 18:15:27 +0900

    style: 필요없는 부분 삭제

--- pages/components/header.js
+++ pages/components/header.js
@@ -32,9 +32,7 @@ export default function Header() {
           <Link href="/projects" legacyBehavior>
             <a className="mr-5 hover:text-gray-900 font-semibold">프로젝트</a>
           </Link>
-          <Link href="/contect" legacyBehavior>
-            <a className="mr-5 hover:text-gray-900 font-semibold">연락하기</a>
-          </Link>
+          <a href="https://open.kakao.com/o/sHuAf7Qe" className="mr-5 hover:text-gray-900 font-semibold">연락하기</a>
         </nav>
         <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">
           Button
