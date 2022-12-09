9e862bc560ec49ba29e04fd5faadfa6502ae32e7
minsoo <als494@naver.com>
2022-12-10 03:56:47 +0900

    feat: 깃허브가 private일 때 들어가지는 페이지와 레이아웃 설정

--- pages/404.js
+++ pages/404.js
@@ -1,7 +1,9 @@
+import Layout from "./components/layout";
+
 export default function PageNotFound() {
   return (
-    <div>
+    <Layout>
       <h1>페이지를 찾을 수 없습니다.</h1>
-    </div>
+    </Layout>
   );
 }
---
+++ pages/private.js
@@ -0,0 +1,9 @@
+import Layout from "./components/layout";
+
+export default function Private() {
+  return (
+    <Layout>
+      <h1>깃허브가 private입니다.</h1>
+    </Layout>
+  );
+}
