019108f658ade43454747502184bd9a69de1e475
minsoo <als494@naver.com>
2023-01-08 21:17:16 +0900

    style: 메인 화면 내용 변경

--- pages/Home/main.js
+++ pages/Home/main.js
@@ -10,29 +10,27 @@ export default function Main() {
         </div>
         <div className="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
           <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
-            안녕하세요 신민수입니다!
-            <br className="hidden lg:inline-block" />
-            환영합니다!
+            소통을 중요시하는 FE 개발자 신민수입니다!
           </h1>
+          <p className="leading-relaxed">
+            &nbsp;&nbsp;&nbsp;&nbsp;▪ 빠르게 무언가를 이뤄내는 것보다 꾸준히 배우며 성장하는 것을 좋아합니다.
+          </p>
+          <p className="leading-relaxed">
+            &nbsp;&nbsp;&nbsp;&nbsp;▪ 어려운 문제를 마주쳐도 끈기 있게 매달리고, 그 과정들에서 성취감과 즐거움을
+            느낍니다.
+          </p>
           <p className="mb-8 leading-relaxed">
-            Copper mug try-hard pitchfork pour-over freegan heirloom neutra air
-            plant cold-pressed tacos poke beard tote bag. Heirloom echo park
-            mlkshk tote bag selvage hot chicken authentic tumeric truffaut
-            hexagon try-hard chambray.
+            &nbsp;&nbsp;&nbsp;&nbsp;▪ 소통을 중요하게 생각하며 여러 번의 팀 프로젝트를 통해 협업 능력을 길러왔습니다.
           </p>
           <div className="flex justify-center">
             <Link href="/teamProjects">
               <button className="btn-project">팀 프로젝트 보러가기</button>
             </Link>
             <Link href="/toyProjects">
-              <button className="btn-project ml-4">
-                개인 프로젝트 보러가기
-              </button>
+              <button className="btn-project ml-4">개인 프로젝트 보러가기</button>
             </Link>
             <a href="https://github.com/Minsoo7044">
-              <button className="btn-project ml-4">
-                깃허브 보러가기
-              </button>
+              <button className="btn-project ml-4">깃허브 보러가기</button>
             </a>
           </div>
         </div>
